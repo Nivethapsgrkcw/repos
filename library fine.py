59a0df42ba3d344a3f0055bd78524b2caf52640a
Nivethapsgrkcw <121852626+Nivethapsgrkcw@users.noreply.github.com>
2023-02-27 16:33:16 +0530

    Add files via upload

---
+++ library fine.py
@@ -0,0 +1,15 @@
+days=int(input("Enter the no.of days:"))
+if days-15==0 :
+    fine=0
+    print("ON due day: No fine ",fine)
+elif days-15<=5:
+    fine=(days-15)*0.70
+    print("the fine amount:",float(fine))
+elif days-15>=5 and days-15<=10:
+    fine=(days-15)*1.80
+    print("The fine amount :",float(fine))
+else :
+    fine=(days-15)*100
+    print("The fine amount :",int(fine))
+    
+
